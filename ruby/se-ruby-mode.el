97ab7c171ebf4450269e47561506e1e98c30749d
Carl Olson <carl-olson@uiowa.edu>
2015-10-07 16:01:41 -0500

    add ruby selection methods

--- ruby/se-ruby-mode.el
+++ ruby/se-ruby-mode.el
@@ -5,9 +5,8 @@
   (concat (file-name-directory load-file-name) "se_ruby_main.rb")
   "Program to run for se-ruby mode.")
 
-(define-derived-mode se-ruby-mode ruby-mode
-  "se-Ruby" "Major mode for Ruby files."
-  
+(define-derived-mode se-ruby-mode ruby-mode "se-Ruby"
+  "Major mode for Ruby files."
   (se-mode)
   (se-inf-start
    (or (get-buffer-process "*se-ruby-mode*") ;; reuse if existing process
@@ -15,8 +14,20 @@
 		      "irb" "--inf-ruby-mode" "-r" se-ruby-program-name)))
   (add-hook 'se-navigation-mode-hook #'se-ruby-parse-file nil t))
 
+(se-navi-define-key 'se-ruby-mode (kbd "m") #'se-ruby-select-method)
+(se-navi-define-key 'se-ruby-mode (kbd "c") #'se-ruby-select-class)
+
 (defun se-ruby-parse-file ()
   (when se-navigation-mode
     (se-inf-parse-file)))
+
+(defun se-ruby-select-method ()
+  (interactive)
+  (or (se-mode-select-name "def")
+      (se-mode-select-name "defs")))
+
+(defun se-ruby-select-class ()
+  (interactive)
+  (se-mode-select-name "class"))
 
 (provide 'se-ruby-mode)
