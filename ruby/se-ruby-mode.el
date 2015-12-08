2db0773bc646b338548fbdfac53f55dba010a3bd
Carl Olson <carl.anders.olson@gmail.com>
2015-12-07 22:32:50 -0600

    use se-create-mode

--- ruby/se-ruby-mode.el
+++ ruby/se-ruby-mode.el
@@ -1,25 +1,19 @@
 
 (require 'se-mode)
+(eval-when-compile (require 'se-macros))
 
 (defvar se-ruby-program-name
   (concat (file-name-directory load-file-name) "se_ruby_main.rb")
   "Program to run for se-ruby mode.")
 
-(define-derived-mode se-ruby-mode ruby-mode "se-Ruby"
-  "Major mode for Ruby files."
-  (se-mode)
+(se-create-mode "Ruby" ruby-mode
   (se-inf-start
    (or (get-buffer-process "*se-ruby-mode*") ;; reuse if existing process
        (start-process "se-ruby-mode" "*se-ruby-mode*"
-		      "irb" "--inf-ruby-mode" "-r" se-ruby-program-name)))
-  (add-hook 'se-navigation-mode-hook #'se-ruby-parse-file nil t))
+   		      "irb" "--inf-ruby-mode" "-r" se-ruby-program-name))))
 
 (se-navi-define-key 'se-ruby-mode (kbd "m") #'se-ruby-select-method)
 (se-navi-define-key 'se-ruby-mode (kbd "c") #'se-ruby-select-class)
-
-(defun se-ruby-parse-file ()
-  (when se-navigation-mode
-    (se-inf-parse-file)))
 
 (defun se-ruby-select-method ()
   (interactive)
