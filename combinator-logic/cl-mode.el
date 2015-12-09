e38bd32c6b2d55b6a54b92b2874eadbd791feda5
Carl Olson <carl.anders.olson@gmail.com>
2015-12-08 19:14:14 -0600

    update cl-mode to use se-create-macro

--- combinator-logic/cl-mode.el
+++ combinator-logic/cl-mode.el
@@ -1,48 +1,33 @@
 
 (require 'se-mode)
+(eval-when-compile (require 'se-macros))
 
 (defvar cl-program-name
-  (concat (file-name-directory load-file-name) "cl-mode.sh")
-  "Location cl-mode.sh script.")
+  (concat (file-name-directory load-file-name) "cl-mode")
+  "Location cl-mode script.")
 
-;; keep emacs version <24 compatability
-(defalias 'cl-parent-mode
-  (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
-
-(define-derived-mode cl-mode cl-parent-mode
-  "CL"
+(se-create-mode "CL" nil
   "Major mode for editing combinator logic files."
-
-  (setq-local comment-start "--")
-  
-  (se-mode)
   (se-inf-start
    (or (get-buffer-process "*cl-mode*")
-       (start-process "cl-mode" "*cl-mode*" "sh" cl-program-name)))
-  (add-hook 'se-navigation-mode-hook #'cl-parse-file nil t)
-  (add-hook 'se-inf-response-hook #'cl-process-message nil t))
+       (start-process "cl-mode" "*cl-mode*" "bash" cl-program-name)))
+  (add-hook 'se-inf-response-hook #'cl-process-message nil t)
+  (setq-local comment-start "--"))
+
+(add-to-list 'auto-mode-alist (cons "\\.cl\\'" #'cl-mode))
 
 (define-key cl-mode-map (kbd "C-c C-x") #'cl-eval-expr)
 (se-navi-define-key 'cl-mode (kbd "x") #'cl-se-eval-selected)
 
-(add-to-list 'auto-mode-alist (cons "\\.cl\\'" #'cl-mode))
-
-(defun cl-parse-file ()
-  "Only parse when navigation mode is active. This prevents the
-navigation mode hook from calling `se-inf-parse-file' when
-deactivating."
-  (when se-navigation-mode
-    (se-inf-parse-file)))
-
-(defun cl-process-message (JSON)
+(defun cl-process-message (json)
   (let ((msg (cdr (assoc 'message json))))
     (when msg
       (message "%s" msg))))
 
-(defun cl-eval-expr (EXPR)
+(defun cl-eval-expr (expr)
   "Evaluates combinatory logic expression."
   (interactive "MCL Eval: ")
-  (se-inf-ask (concat "--eval 'print " EXPR "' --timeout 1 --spans")))
+  (se-inf-ask (concat "--timeout 1 --spans --eval\nprint " expr)))
 
 (defun cl-se-eval-selected ()
   (interactive)
